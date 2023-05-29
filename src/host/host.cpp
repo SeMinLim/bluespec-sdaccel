5a7fa1a1248cac07056674e65adae45004f69d6a
SeMinLim <seminl1@ics.uci.edu>
2023-05-29 01:20:25 -0700

    solved an issue of executing host binary

--- src/host/host.cpp
+++ src/host/host.cpp
@@ -8,6 +8,7 @@ Associated Filename: main.c
 #         memory inplace.
 *******************************************************************************/
 
+//#include <iostream>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,7 +26,7 @@ Associated Filename: main.c
 #include "xclhal2.h"
 
 #include "opencl_helpers.h"
-//#include <iostream>
+
 ////////////////////////////////////////////////////////////////////////////////
 
 #define NUM_WORKGROUPS (1)
@@ -42,55 +43,48 @@ Associated Filename: main.c
 
 int main(int argc, char* argv[])
 {
-	cl_int err;				// error code returned from api calls
-	cl_context context;			// compute context
-	cl_command_queue commands;		// compute command queue
-	cl_program program;			// compute programs
-	cl_kernel kernel;			// compute kernel
+	cl_int err;				// Error code returned from api calls
+	cl_context context;			// Compute context
+	cl_command_queue commands;		// Compute command queue
+	cl_program program;			// Compute programs
+	cl_kernel kernel;			// Compute kernel
 	init_setup(0, context, commands, program, kernel);
+	
+	int* h_data;				// Host memory for input vector
+	int h_B_output[MAX_LENGTH];		// Host memory for output vector
+	cl_mem d_A, d_B;			// Device memory used for a vector
 
-    	int* h_data;				// host memory for input vector
-	cl_mem d_A;				// device memory used for a vector
-	int h_B_output[MAX_LENGTH];		// host memory for output vector
-	cl_mem d_B;				// device memory used for a vector
+	const uint data_bytes = (16*1024);		// 16KB of data
+	h_data = (int*)malloc(sizeof(int)*MAX_LENGTH);	// 8KB
 
-    	// Create structs to define memory bank mapping
-    	cl_mem_ext_ptr_t mem_ext;
-    	mem_ext.obj = NULL;
-    	mem_ext.param = kernel;
+	// For allocating buffer to specific global memory bank
+	// User has to use cl_mem_ext_ptr_t and provide the banks
+	cl_mem_ext_ptr_t mem_ext_A, mem_ext_B;
+	
+	mem_ext_A.obj = NULL;
+	mem_ext_A.param = 0;
+	mem_ext_A.flags = 1 | XCL_MEM_TOPOLOGY;
+
+	mem_ext_B.obj = NULL;
+	mem_ext_B.param = 0;
+	mem_ext_B.flags = 2 | XCL_MEM_TOPOLOGY;
     
-
-	const uint data_bytes = (256*1024*1024);
-
-	h_data = (int*)malloc(data_bytes);
-
-	mem_ext.flags = 1 | XCL_MEM_TOPOLOGY;
-	d_A = clCreateBuffer(context,  CL_MEM_READ_WRITE | CL_MEM_EXT_PTR_XILINX,  data_bytes, &mem_ext, NULL);
-
-	if (!(d_A)) {
-		printf("Error: Failed to allocate device memory A!\n");
+	// Creating buffers
+	d_A = clCreateBuffer(context,  CL_MEM_READ_WRITE | CL_MEM_EXT_PTR_XILINX,  data_bytes, &mem_ext_A, NULL);
+	d_B = clCreateBuffer(context,  CL_MEM_READ_WRITE | CL_MEM_EXT_PTR_XILINX,  data_bytes, &mem_ext_B, NULL);
+	if (!(d_A&&d_B)) {
+		printf("Error: Failed to allocate device memory!\n");
 		printf("Test failed\n");
 		return EXIT_FAILURE;
 	}
 
-	mem_ext.flags = 2 | XCL_MEM_TOPOLOGY;
-	d_B = clCreateBuffer(context,  CL_MEM_READ_WRITE | CL_MEM_EXT_PTR_XILINX,  data_bytes, &mem_ext, NULL);
-
-	if (!(d_B)) {
-		printf("Error: Failed to allocate device memory B!\n");
-		printf("Test failed\n");
-		return EXIT_FAILURE;
-	}
-
-
+	//
 	err = clEnqueueWriteBuffer(commands, d_A, CL_TRUE, 0, data_bytes, h_data, 0, NULL, NULL);
 	if (err != CL_SUCCESS) {
 		printf("Error: Failed to write to source array h_data!\n");
 		printf("Test failed\n");
 		return EXIT_FAILURE;
 	}
-
-
 	err = clEnqueueWriteBuffer(commands, d_B, CL_TRUE, 0, data_bytes, h_data, 0, NULL, NULL);
 	if (err != CL_SUCCESS) {
 		printf("Error: Failed to write to source array h_data!\n");
@@ -98,69 +92,66 @@ int main(int argc, char* argv[])
 		return EXIT_FAILURE;
 	}
 
-
 	// Set the arguments to our compute kernel
 	err = 0;
-	cl_uint d_scalar00 = data_bytes/64;
+	cl_uint d_scalar00 = (256/64)*1024*1024;
 	err |= clSetKernelArg(kernel, 0, sizeof(cl_uint), &d_scalar00); // Not used in example RTL logic.
 	err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_A); 
 	err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_B); 
-
 	if (err != CL_SUCCESS) {
 		printf("Error: Failed to set kernel arguments! %d\n", err);
 		printf("Test failed\n");
 		return EXIT_FAILURE;
 	}
 
-	
 	// Execute the kernel over the entire range of our 1d input data set
 	// using the maximum number of work group items for this device
-	printf( "!!!!\n"); fflush(stdout);
+	printf( "[Execute] Start\n"); fflush(stdout);
 	err = clEnqueueTask(commands, kernel, 0, NULL, NULL);
 	if (err) {
 		printf("Error: Failed to execute kernel! %d\n", err);
 		printf("Test failed\n");
 		return EXIT_FAILURE;
 	}
-	printf( "2222\n"); fflush(stdout);
-	
-	// Read back the results from the device to verify the output
+	printf( "[Execute] Running\n"); fflush(stdout);
+    
+	// Check the kernel termination
 	cl_event readevent;
 	clFinish(commands);
-	printf( "3333\n"); fflush(stdout);
+	printf( "[Execute] Finished\n"); fflush(stdout);
 	sleep(5);
 
+	// Read back the restuls from the device to verify the output
 	err = 0;
 	err |= clEnqueueReadBuffer( commands, d_A, CL_TRUE, 0, data_bytes, h_B_output, 0, NULL, &readevent );
-
-	printf( "4444\n"); fflush(stdout);
+	printf( "[Result] Memory Reading Start\n"); fflush(stdout);
 	if (err != CL_SUCCESS) {
 		printf("Error: Failed to read output array! %d\n", err);
 		printf("Test failed\n");
 		return EXIT_FAILURE;
 	}
 	clWaitForEvents(1, &readevent);
-	printf( "5555\n"); fflush(stdout);
+	printf( "[Result] Memory Reading Finished\n"); fflush(stdout);
 	
 	// Check Results
 	for (uint i = 0; i < 20; i++) {
 		printf( "%x \t %x\n", h_data[i], h_B_output[i] );
 	}
-    	//--------------------------------------------------------------------------
-    	// Shutdown and cleanup
-    	//-------------------------------------------------------------------------- 
-    	clReleaseMemObject(d_A);
-    	clReleaseMemObject(d_B);
-    	clReleaseProgram(program);
-    	clReleaseKernel(kernel);
-    	clReleaseCommandQueue(commands);
-    	clReleaseContext(context);
+	//--------------------------------------------------------------------------
+	// Shutdown and cleanup
+	//-------------------------------------------------------------------------- 
+	clReleaseMemObject(d_A);
+	clReleaseMemObject(d_B);
+	clReleaseProgram(program);
+	clReleaseKernel(kernel);
+	clReleaseCommandQueue(commands);
+	clReleaseContext(context);
 
-    	if (false) {
-        	printf("INFO: Test failed\n");
-        	return EXIT_FAILURE;
-    	} else {
-        	printf("INFO: Test completed successfully.\n");
-        	return EXIT_SUCCESS;
-    	}
+	if (false) {
+		printf("INFO: Test failed\n");
+		return EXIT_FAILURE;
+	} else {
+		printf("INFO: Test completed successfully\n");
+		return EXIT_SUCCESS;
+	}
 }
