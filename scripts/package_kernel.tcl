afdc988b09a042e65bfd78c131228098eae553f9
SeMinLim <seminl1@ics.uci.edu>
2023-06-14 06:55:59 -0700

    updated package_kernel.tcl

--- scripts/package_kernel.tcl
+++ scripts/package_kernel.tcl
@@ -1,70 +1,138 @@
-# /*******************************************************************************
-# Copyright (c) 2018, Xilinx, Inc.
-# All rights reserved.
-# 
-# Redistribution and use in source and binary forms, with or without modification,
-# are permitted provided that the following conditions are met:
-# 
-# 1. Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
-# 
-# 
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation
-# and/or other materials provided with the distribution.
-# 
-# 
-# 3. Neither the name of the copyright holder nor the names of its contributors
-# may be used to endorse or promote products derived from this software
-# without specific prior written permission.
-# 
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,THE IMPLIED 
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
-# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# *******************************************************************************/
+# Copyright (C) 2019-2021 Xilinx, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may
+# not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
 
 set path_to_hdl "./src/kernel/obj/verilog"
 set path_to_packaged "./packaged_kernel_${suffix}"
 set path_to_tmp_project "./tmp_kernel_pack_${suffix}"
 
 create_project -force kernel_pack $path_to_tmp_project 
-add_files [glob $path_to_hdl/*.v]
+add_files -norecurse [glob $path_to_hdl/*.v]
 update_compile_order -fileset sources_1
 update_compile_order -fileset sim_1
 set_property top mkKernelTop [current_fileset]
-
 ipx::package_project -root_dir $path_to_packaged -vendor xilinx.com -library RTLKernel -taxonomy /KernelIP -import_files -set_current false
 ipx::unload_core $path_to_packaged/component.xml
 ipx::edit_ip_in_project -upgrade true -name tmp_edit_project -directory $path_to_packaged $path_to_packaged/component.xml
-set_property core_revision 2 [ipx::current_core]
-foreach up [ipx::get_user_parameters] {
-  ipx::remove_user_parameter [get_property NAME $up] [ipx::current_core]
-}
-set_property sdx_kernel true [ipx::current_core]
-set_property sdx_kernel_type rtl [ipx::current_core]
-ipx::create_xgui_files [ipx::current_core]
-ipx::associate_bus_interfaces -busif m00_axi -clock ap_clk [ipx::current_core]
-ipx::associate_bus_interfaces -busif m01_axi -clock ap_clk [ipx::current_core]
-#ipx::associate_bus_interfaces -busif m02_axi -clock ap_clk [ipx::current_core]
-#ipx::associate_bus_interfaces -busif m03_axi -clock ap_clk [ipx::current_core]
-#ipx::associate_bus_interfaces -busif m04_axi -clock ap_clk [ipx::current_core]
-#ipx::associate_bus_interfaces -busif m05_axi -clock ap_clk [ipx::current_core]
-#ipx::associate_bus_interfaces -busif m06_axi -clock ap_clk [ipx::current_core]
-#ipx::associate_bus_interfaces -busif m07_axi -clock ap_clk [ipx::current_core]
-ipx::associate_bus_interfaces -busif s_axi_control -clock ap_clk [ipx::current_core]
 
-set_property xpm_libraries {XPM_CDC XPM_MEMORY XPM_FIFO} [ipx::current_core]
-set_property supported_families { } [ipx::current_core]
-set_property auto_family_support_level level_2 [ipx::current_core]
-ipx::update_checksums [ipx::current_core]
-ipx::save_core [ipx::current_core]
+set core [ipx::current_core]
+
+set_property core_revision 2 $core
+foreach up [ipx::get_user_parameters] {
+  ipx::remove_user_parameter [get_property NAME $up] $core
+}
+ipx::associate_bus_interfaces -busif m00_axi -clock ap_clk $core
+ipx::associate_bus_interfaces -busif m01_axi -clock ap_clk $core
+ipx::associate_bus_interfaces -busif s_axi_control -clock ap_clk $core
+
+set mem_map    [::ipx::add_memory_map -quiet "s_axi_control" $core]
+set addr_block [::ipx::add_address_block -quiet "reg0" $mem_map]
+
+set reg      [::ipx::add_register "CTRL" $addr_block]
+  set_property description    "Control signals"    $reg
+  set_property address_offset 0x000 $reg
+  set_property size           32    $reg
+set field [ipx::add_field AP_START $reg]
+  set_property ACCESS {read-write} $field
+  set_property BIT_OFFSET {0} $field
+  set_property BIT_WIDTH {1} $field
+  set_property DESCRIPTION {Control signal Register for 'ap_start'.} $field
+  set_property MODIFIED_WRITE_VALUE {modify} $field
+set field [ipx::add_field AP_DONE $reg]
+  set_property ACCESS {read-only} $field
+  set_property BIT_OFFSET {1} $field
+  set_property BIT_WIDTH {1} $field
+  set_property DESCRIPTION {Control signal Register for 'ap_done'.} $field
+  set_property READ_ACTION {modify} $field
+set field [ipx::add_field AP_IDLE $reg]
+  set_property ACCESS {read-only} $field
+  set_property BIT_OFFSET {2} $field
+  set_property BIT_WIDTH {1} $field
+  set_property DESCRIPTION {Control signal Register for 'ap_idle'.} $field
+  set_property READ_ACTION {modify} $field
+set field [ipx::add_field AP_READY $reg]
+  set_property ACCESS {read-only} $field
+  set_property BIT_OFFSET {3} $field
+  set_property BIT_WIDTH {1} $field
+  set_property DESCRIPTION {Control signal Register for 'ap_ready'.} $field
+  set_property READ_ACTION {modify} $field
+set field [ipx::add_field RESERVED_1 $reg]
+  set_property ACCESS {read-only} $field
+  set_property BIT_OFFSET {4} $field
+  set_property BIT_WIDTH {3} $field
+  set_property DESCRIPTION {Reserved.  0s on read.} $field
+  set_property READ_ACTION {modify} $field
+set field [ipx::add_field AUTO_RESTART $reg]
+  set_property ACCESS {read-write} $field
+  set_property BIT_OFFSET {7} $field
+  set_property BIT_WIDTH {1} $field
+  set_property DESCRIPTION {Control signal Register for 'auto_restart'.} $field
+  set_property MODIFIED_WRITE_VALUE {modify} $field
+set field [ipx::add_field RESERVED_2 $reg]
+  set_property ACCESS {read-only} $field
+  set_property BIT_OFFSET {8} $field
+  set_property BIT_WIDTH {24} $field
+  set_property DESCRIPTION {Reserved.  0s on read.} $field
+  set_property READ_ACTION {modify} $field
+
+set reg      [::ipx::add_register "GIER" $addr_block]
+  set_property description    "Global Interrupt Enable Register"    $reg
+  set_property address_offset 0x004 $reg
+  set_property size           32    $reg
+
+set reg      [::ipx::add_register "IP_IER" $addr_block]
+  set_property description    "IP Interrupt Enable Register"    $reg
+  set_property address_offset 0x008 $reg
+  set_property size           32    $reg
+
+set reg      [::ipx::add_register "IP_ISR" $addr_block]
+  set_property description    "IP Interrupt Status Register"    $reg
+  set_property address_offset 0x00C $reg
+  set_property size           32    $reg
+
+set reg      [::ipx::add_register -quiet "a" $addr_block]
+  set_property address_offset 0x010 $reg
+  set_property size           [expr {8*8}]   $reg
+  set regparam [::ipx::add_register_parameter -quiet {ASSOCIATED_BUSIF} $reg] 
+  set_property value m_axi_gmem $regparam 
+
+set reg      [::ipx::add_register -quiet "b" $addr_block]
+  set_property address_offset 0x01C $reg
+  set_property size           [expr {8*8}]   $reg
+  set regparam [::ipx::add_register_parameter -quiet {ASSOCIATED_BUSIF} $reg] 
+  set_property value m_axi_gmem $regparam 
+
+set reg      [::ipx::add_register -quiet "c" $addr_block]
+  set_property address_offset 0x028 $reg
+  set_property size           [expr {8*8}]   $reg
+  set regparam [::ipx::add_register_parameter -quiet {ASSOCIATED_BUSIF} $reg] 
+  set_property value m_axi_gmem $regparam 
+
+set reg      [::ipx::add_register -quiet "length_r" $addr_block]
+  set_property address_offset 0x034 $reg
+  set_property size           [expr {4*8}]   $reg
+
+set_property slave_memory_map_ref "s_axi_control" [::ipx::get_bus_interfaces -of $core "s_axi_control"]
+
+set_property xpm_libraries {XPM_CDC XPM_MEMORY XPM_FIFO} $core
+set_property sdx_kernel true $core
+set_property sdx_kernel_type rtl $core
+set_property supported_families { } $core
+set_property auto_family_support_level level_2 $core
+ipx::create_xgui_files $core
+ipx::update_checksums $core
+ipx::check_integrity -kernel $core
+ipx::save_core $core
 close_project -delete
